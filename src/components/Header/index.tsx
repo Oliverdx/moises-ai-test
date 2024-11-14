1bae03c13a9b122a4f08b75a50fda10a4cd4a7d5
Vanderlei Oliveira <vanderlei.oliveira@oowlish.com>
2024-11-14 15:02:42 -0300

    repsonsive menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,64 @@
+"use client"
+
 import { link } from "@/types/global";
 
 import styles from "./style.module.scss";
 import { Logo } from "@/icons/Logo";
+import { useState } from "react";
 
-const Header = ({links}: {links: Array<link>}) => {
+const Header = ({ links }: { links: Array<link> }) => {
 
-  const renderLink = (id: number, link: string, title: string | JSX.Element) =>
-  <li key={id}>
-    <a href={link}>{title}</a>
-  </li>
-  
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const loginLink = links.find(link => link.link === "/login");
+
+  const toggleMenu = () => {
+    const body = document.getElementsByTagName('body')[0];
+    body.style.overflow = !mobileOpen ? "hidden" : "";
+
+    setMobileOpen(!mobileOpen);
+  }
+
+  const renderLink = (id: number, link: string, title: string | JSX.Element, openNewWindow: boolean) =>
+    <li key={id}>
+      <a
+        href={link || "#"}
+        target={openNewWindow ? "_blank" : "_self"}
+      >{title}</a>
+    </li>
+
   return <header className={styles.header}>
-    <nav className={styles.headerWrapper}>
-      <ul>
-        {renderLink(0, '/', <Logo />)}
-        {links?.map(link => renderLink(link.id, link.link, link.title))}
-      </ul>
-    </nav>
+    <div className={styles.headerWrapper}>
+      <div className={styles.logoWrapper}>
+        <a href="/">
+          <Logo />
+        </a>
+      </div>
+
+      <div className={`${styles.hamburgerMenu} ${mobileOpen ? styles.menuOpen : ""}`} onClick={() => toggleMenu()}>
+        <span></span>
+      </div>
+
+      <div className={`${styles.menu} ${mobileOpen ? styles.menuOpen : ""}`}>
+        <nav className={styles.menuWrapper}>
+          <ul>
+            {links?.map(link => {
+              if (link.title.toLowerCase() !== "login")
+                return renderLink(link.id, link.link, link.title, link.openNewWindow)
+            })}
+          </ul>
+        </nav>
+
+        {loginLink && <div className={styles.loginWrapper}>
+          <a
+            href={loginLink.link}
+            target={loginLink.openNewWindow ? "_blank" : "_self"}
+          >
+            {loginLink.title}
+          </a>
+        </div>}
+      </div>
+    </div>
   </header>
 };
 
