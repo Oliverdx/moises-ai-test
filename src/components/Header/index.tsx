5c5a1320a3fbb052ee91fb7eae248345ebd656c9
Vanderlei Oliveira <vanderlei.oliveira@oowlish.com>
2024-11-14 10:02:59 -0300

    fix header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,23 @@
 import { link } from "@/types/global";
 
 import styles from "./style.module.scss";
+import { Logo } from "@/icons/Logo";
 
 const Header = ({links}: {links: Array<link>}) => {
 
-  const renderLink = (id: number, link: string, title: string) =>
+  const renderLink = (id: number, link: string, title: string | JSX.Element) =>
   <li key={id}>
     <a href={link}>{title}</a>
   </li>
   
-  return <nav className={styles.header}>
-    <ul>
-      {renderLink(0, '/', 'Developer')}
-      {links?.map(link => renderLink(link.id, link.link, link.title))}
-    </ul>
-  </nav>
+  return <header className={styles.header}>
+    <nav className={styles.headerWrapper}>
+      <ul>
+        {renderLink(0, '/', <Logo />)}
+        {links?.map(link => renderLink(link.id, link.link, link.title))}
+      </ul>
+    </nav>
+  </header>
 };
 
 export default Header;
