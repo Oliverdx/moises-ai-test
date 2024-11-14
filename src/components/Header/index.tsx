e033810f8cbc4f77327ef7ba7a781449c3b9ff38
Vanderlei Oliveira <vanderlei.oliveira@oowlish.com>
2024-11-14 19:40:00 -0300

    deploys path test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { link } from "@types/global";
 
 import styles from "./style.module.scss";
-import { Logo } from "@icons/Logo";
+import { Logo } from "@icons/Logo.tsx";
 import Link from "next/link";
 
 const Header = ({ links }: { links: Array<link> }) => {
