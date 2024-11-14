309829d1b1254f8f6b255e8abe5e50d3920f619a
Vanderlei Oliveira <vanderlei327@gmail.com>
2024-11-14 20:13:59 -0300

    fix undefined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 import SECTION_COMPONENTS from "@/components/SectionComponents";
 import { sectionBase } from "@/types/sections";
 import { fetchData } from "@/utils/fetchData";
-import { Suspense} from "react";
+import { Suspense } from "react";
 
 
 export default async function Home() {
 
   const content = await fetchData('pages');
 
+  if (!content)
+    return null;
+
   return (
     <>
       {content[0]?.contentSections?.map((section: sectionBase) => {
-    
         const SectionComponent = SECTION_COMPONENTS[section?.__component.replace('sections.', '')];
 
-        if(!SectionComponent)
+        if (!SectionComponent)
           return null;
 
         return <Suspense key={section.id}>
-          <SectionComponent {...section}/>
+          <SectionComponent {...section} />
         </Suspense>
 
       })}
